dcdb6da1b942b34f5863af9508cc64c41a5b67dd
Valeria Velichko <n0veva00@students.oamk.fi>
2021-03-17 09:21:57 +0200

    valmis harjoitustyö (frontendi)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+const URL = "https://localhost/shoppinglist/";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+  const [amounts, setAmounts] = useState([]);
+  const [amount, setAmount] = useState('');
 
+  useEffect(() => {
+    let status = 0;
+    fetch(URL + 'index.php')
+      .then(res => {
+        status = parseInt(res.status);
+        return res.json()
+      })
+      .then(
+        (res) => {
+          if (status === 200) {
+            setItems(res);
+          } else {
+            alert(res.error);
+          }
+        }, (error) => {
+          alert('Järjestelmässä virhe yritä uudelleen myöhemmin!');
+        }
+      )
+  }, [])
+
+  function save(e) {
+    e.preventDefault();
+    let status = 0;
+    fetch(URL + 'add.php',{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        description: item,
+        amount: amount
+      })
+    })
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json();
+    })
+    .then (
+      (res) => {
+        if (status === 200) {
+          setItems(items => [...items,res]);
+          setItem('');
+          setAmounts(amounts => [...amounts,res]);
+          setAmount('');
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error)
+      }
+    )
+  }
+
+  function remove (id) {
+    let status = 0;
+    fetch(URL + 'delete.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: id
+      })
+    })
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json();
+    })
+    .then (
+      (res) => {
+        if (status === 200) {
+          const newListWithoutRemoved = items.filter((item) => item.id !== id);
+          setItems(newListWithoutRemoved);
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }
+
+  return (
+    <div className="container">
+      <h3>Shopping list</h3>
+      <div>
+        <form onSubmit={save}>
+          <label>New item</label>
+          <input value={item} onChange={e => setItem(e.target.value)} />
+          <input value={amount} onChange={e => setAmount(e.target.value)} />
+          <button>Add</button>
+        </form>
+      </div>
+        <ol>
+          {items.map(item => (
+            <li key = {item.id}>
+              {item.description}
+              {item.amount}
+                <a className='delete' onClick={() => remove(item.id)} href= '#'> 
+                Delete
+                </a>
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
+  }
 export default App;
